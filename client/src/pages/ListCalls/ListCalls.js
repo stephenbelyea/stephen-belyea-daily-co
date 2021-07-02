5d84a50b0fe38d21fd40ee5a5f03422c19280f69
Stephen Belyea <belyea.stephen@gmail.com>
2021-07-02 17:03:49 -0400

    Dash main calls list fetched and displayed with link to metrics page

--- client/src/pages/ListCalls/ListCalls.js
+++ client/src/pages/ListCalls/ListCalls.js
@@ -1,16 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ErrorMessage } from '../../components';
+import { ROUTES } from '../../constants';
 
 export default function ListCalls({ service }) {
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-  
+  const [calls, setCalls] = useState([]);
+
+  const getLocaleDate = (timestamp) => {
+    const d = new Date(timestamp).toUTCString();
+    return d.toLocaleString();
+  }
+
+  useEffect(async () => {
+    setError('');
+    setLoading(true);
+
+    try {
+      const allCalls = await service.getAllCalls();
+      setCalls(allCalls);
+    } catch (error) {
+      setError('Unable to fetch calls our call service.');
+    }
+
+    setLoading(false);
+  }, []);
+
   return (
     <div className="list-calls">
       <h1>Calls Dashboard</h1>
-      <div>
-        
-      </div>
       <ErrorMessage message={error} />
+      {loading ? <p>Loading...</p> : (
+        <table>
+          <thead>
+            <tr>
+              <th>Started</th>
+              <th>Length</th>
+              <th>Room</th>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            {calls.length > 0 ? (
+              calls.map(call => (
+                <tr key={call.id}>
+                  <td>{getLocaleDate(call.start)}</td>
+                  <td>{call.length} min</td>
+                  <td>{call.room}</td>
+                  <td>
+                    <Link to={`${ROUTES.DASH_CALL_METRICS}/${call.id}`}>
+                      View metrics
+                    </Link>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr><td>No calls found.</td></tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
--- client/src/services/CallService.js
+++ client/src/services/CallService.js
@@ -6,6 +6,15 @@ function mapStartCallData(data) {
   };
 }
 
+function mapAllCallsData({ data }) {
+  return data.map(call => ({
+    id: call.id,
+    room: call.room,
+    start: call.start_time * 1000,
+    length: Math.ceil(call.duration / 60)
+  }));
+}
+
 class CallService {
   constructor() {
     this.instance = axios.create({
@@ -17,6 +26,15 @@ class CallService {
     try {
       const response = await this.instance.post('/rooms');
       return mapStartCallData(response.data);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getAllCalls() {
+    try {
+      const response = await this.instance.get('/meetings');
+      return mapAllCallsData(response.data);
     } catch (error) {
       throw new Error(error);
     }
