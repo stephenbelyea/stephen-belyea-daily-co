ded0bf187e7fb2f1db39da628d42626670d065c4
Stephen Belyea <belyea.stephen@gmail.com>
2021-07-02 12:58:02 -0400

    Pass-through Express API for Daily calls, start readme and example env

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# stephen-belyea-daily
+# Stephen Belyea | Daily.co Metrics
+
+## Getting started
+
+You will need Git, Node.js, and NPM to pull, install, and run this project.
+
+**Mandatory:** Add your `DAILY_API_KEY` to the `.env` file in the project root.
+
+## Server
+
+Install and run the API locally:
+
+```
+cd server
+npm install
+npm start
+```
---
+++ server/server.js
@@ -0,0 +1,68 @@
+require('dotenv').config({ path: '../.env' });
+const axios = require("axios");
+const express = require('express');
+
+const API_KEY = process.env.DAILY_API_KEY;
+const BASE_URL = process.env.DAILY_API_BASE_URL;
+const PORT = process.env.PORT;
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { 
+    Authorization: `Bearer ${API_KEY}` 
+  }
+});
+
+const apiHelper = async (method, url, data = {}) => {
+  try {
+    const response = await api.request({ 
+      method, 
+      url, 
+      data 
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const app = express();
+app.use(express.json());
+
+app.get("/info", async (_, response) => {
+  try {
+    const data = await apiHelper("get", "/");
+    response.json(data);
+  } catch (e) {
+    response.status(500).json({ 
+      error: e.message 
+    });
+  }
+});
+
+app.get("/meetings", async (_, response) => {
+  try {
+    const data = await apiHelper("get", "/meetings");
+    response.json(data);
+  } catch (e) {
+    response.status(500).json({ 
+      error: e.message 
+    });
+  }
+});
+
+app.post("/rooms", async (request, response) => {
+  try {
+    const data = await apiHelper("post", "/rooms", request.body);
+    response.json(data);
+  } catch (e) {
+    response.status(500).json({ 
+      error: e.message 
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`)
+});
+
